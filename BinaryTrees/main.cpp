4eb3fc070decd97baea890a65cd3793c4325a14a
mscott2016 <mscott2016@my.fit.edu>
2018-02-08 11:00:29 -0500

    Initial Commit

---
+++ BinaryTrees/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinaryTrees
+//
+//  Created by Marley on 2/8/18.
+//  Copyright © 2018 Mar Z Mar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
